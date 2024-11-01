58ce488a815d455e9fbb04f951fad11adbae7d14
unknown <vatlemin@gmail.com>
2024-11-01 09:23:48 +0000

    tt2

--- backendKYC/ecashh/appecash/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Personne
-
-class PersonneAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Personne._meta.fields if field.name != "id"]  # Affiche tous les champs sauf 'id'
-    search_fields = ['NNI', 'prenom_fr', 'nom_famille_fr']  # Champs de recherche
-
-admin.site.register(Personne, PersonneAdmin)
--- backendKYC/ecashh/ecashh/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from ecashh import settings
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # URL pour l'interface d'administration de Django
-    path('appecash/', include('appecash.urls'))  # Inclure les URL définies dans votre application 'api'
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
